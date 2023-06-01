a4a6ab00b34d35a0a6fb14342329ef8d00b0cc41
Adrian Montano <adrian.montano2011@yahoo.com>
2023-06-01 09:18:57 -0400

    Se refactorizó la interfaz del usuario segun lo requerido por el cliente

--- src/devolucion.test.js
+++ src/devolucion.test.js
@@ -4,4 +4,7 @@ describe("Devolucion", () => {
   it("deberia restar dos numeros", () => {
     expect(restar(3, 2)).toEqual(1);
   });
+  it("deberia restar dos numeros", () => {
+    expect(restar(3, 2)).toEqual(1);
+  });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -12,5 +12,5 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + restar(firstNumber, secondNumber) + "</p>";
 });
