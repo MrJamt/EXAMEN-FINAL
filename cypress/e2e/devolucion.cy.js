88d3999a676118cac648b26e42799c272d04bbbb
Adrian Montano <adrian.montano2011@yahoo.com>
2023-06-01 09:06:46 -0400

    Se colocaron nombres adecuados en los archivos

--- cypress/e2e/devolucion.cy.js
+++ cypress/e2e/devolucion.cy.js
@@ -1,3 +1,4 @@
+
 describe("Sumador", () => {
   it("Shows the amount of the addition to the user", () => {
     cy.visit("/");
--- src/devolucion.test.js
+++ src/devolucion.test.js
@@ -1,4 +1,4 @@
-import sumar from "./sumador.js";
+import sumar from "./devolucion.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
